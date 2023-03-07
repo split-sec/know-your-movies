c134891dbfd09dc1805aa78cbcbdef0cc140a739
Shyam Venkatesh <shyamvenkat007@gmail.com>
2023-03-07 14:09:58 +0530

    Last working state as of 14:10

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import SampleCard from './Components/SampleCard/SampleCard';
 import SearchResults from './Components/SearchResults/SearchResults';
 
 function App() {
   return (
     <div className="App">
       <SearchResults />
+      <SampleCard />
     </div>
   );
 }
